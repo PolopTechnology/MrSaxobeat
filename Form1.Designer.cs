8fc743d90632cd1c8caf64c6da005d99bdeb591f
PolopTech <73751987+PolopTechnology@users.noreply.github.com>
2021-04-30 16:24:40 -0400

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿
+namespace BatBeat
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox8 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.Temmy = new System.Windows.Forms.Timer(this.components);
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox8
+            // 
+            this.pictureBox8.BackColor = System.Drawing.Color.Red;
+            this.pictureBox8.Location = new System.Drawing.Point(256, 126);
+            this.pictureBox8.Name = "pictureBox8";
+            this.pictureBox8.Size = new System.Drawing.Size(359, 330);
+            this.pictureBox8.TabIndex = 7;
+            this.pictureBox8.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(939, 291);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 48);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "Toggle";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Temmy
+            // 
+            this.Temmy.Enabled = true;
+            this.Temmy.Tick += new System.EventHandler(this.Temmy_Tick);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(256, 51);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(333, 26);
+            this.textBox1.TabIndex = 13;
+            this.textBox1.Text = " Type in your bpm...";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1164, 716);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox8);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox pictureBox8;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer Temmy;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BatBeat
+{
+    public partial class Form1 : Form
+    {
+        int bpm = 0;
+        bool beat = true;
+        bool start = false;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(textBox1.Text == null)
+            {
+                MessageBox.Show("INPUT A BPM!");
+            } 
+            else
+            {
+                bpm = Int32.Parse(textBox1.Text);
+                int f1 = bpm / 60;
+                string f2 = f1.ToString();
+                string[] f3 = f2.Split('.');
+                int f4 = Int32.Parse(f3[0]);
+                int f5 = 1000 / f4;
+                int f6 = f5 / 2;
+                Temmy.Interval = f6;
+                start = true;
+            }
+        }
+
+        private void Temmy_Tick(object sender, EventArgs e)
+        {
+            if(start == true)
+            {
+                if(beat == true)
+                {
+                    pictureBox8.BackColor = Color.Blue;
+                    beat = false;
+                }
+                else if(beat == false)
+                {
+                    pictureBox8.BackColor = Color.Red;
+                    beat = true;
+                }
+            }
+        }
+    }
+}
